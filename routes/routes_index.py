b928537100f403e9a13a37eef058e60e811c0dff
MRROBOT\nicai <czy1996@users.noreply.github.com>
2017-06-27 09:54:01 +0800

    static

--- routes/routes_index.py
+++ routes/routes_index.py
@@ -9,7 +9,16 @@ def route_index(request):
     return r.encode(encoding='utf-8')
 
 
+def route_static(request):
+    filename = request.query.get('file', 'doge.gif')
+    path = 'static/' + filename
+    with open(path, 'rb') as f:
+        header = b'HTTP/1.1 200 OK\r\nContent-Type: image/gif\r\n\r\n'
+        img = header + f.read()
+        return img
+
 route_dict = {
     '/': route_index,
+    '/static': route_static,
 
 }
